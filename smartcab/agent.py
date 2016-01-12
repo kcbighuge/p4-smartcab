05d80806d5d91368163fb629603517439a664b4f
kcbighuge <kcbighuge@gmail.com>
2016-01-12 17:26:15 -0600

    add tracking of optimal policy

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -32,6 +32,7 @@ class LearningAgent(Agent):
 
         ## keep track of results
         self.results = []
+        self.optimal_policy_used = []
 
 
     def reset(self, destination=None):
@@ -39,13 +40,13 @@ class LearningAgent(Agent):
         # TODO: Prepare for a new trip; reset any variables here, if required
         self.reward_sum = 0
         self.current_trial += 1
-        self.epsilon = max(0.95 - .2*(self.current_trial/4), .01)  ## decay epsilon, 100 trials
+        self.epsilon = 0.95 - .2*(self.current_trial/5)  ## decay epsilon, 100 trials
         self.transitions = {}
         self.init_deadline = self.env.get_deadline(self)
+        self.optimal_action_used = []
 
 
     def update(self, t):
-        print '================'
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         print'next_waypoint: {}\n----'.format(self.next_waypoint)  ## [debug]
@@ -83,6 +84,29 @@ class LearningAgent(Agent):
 
         ## Boltzmann exploration: P(a) = exp(Q(s,a) / sum(exp(Q)) / K), decay K over time
 
+        ################################
+        ## keep track of optimal policy
+        best_action_ok = True
+        if self.next_waypoint == 'right':
+            if inputs['light'] == 'red' and inputs['left'] == 'forward':
+                best_action_ok = False
+        elif self.next_waypoint == 'straight':
+            if inputs['light'] == 'red':
+                best_action_ok = False
+        elif self.next_waypoint == 'left':
+            if inputs['light'] == 'red' or (inputs['oncoming'] == 'forward' or inputs['oncoming'] == 'right'):
+                best_action_ok = False
+
+        best_action = None
+        if best_action_ok:
+            best_action = self.next_waypoint
+
+        if best_action == action:
+            self.optimal_action_used.append(1)
+        else:
+            self.optimal_action_used.append(0)
+        ################################
+
         # Execute action and get reward
         reward = self.env.act(self, action)
 
@@ -104,16 +128,23 @@ class LearningAgent(Agent):
         if self.Q[current_state] == [0,0,0,0]:
             self.Q[current_state] = [.5,.5,.5,.5]
             self.Q[current_state][self.A.index(action)] = 0
+
         if t!=0:
             self.Q[self.transitions[t-1][0]][self.transitions[t-1][1]] = \
                 (1-alpha)*self.Q[self.transitions[t-1][0]][self.transitions[t-1][1]] + \
                 (alpha * (self.transitions[t-1][2] + gamma * self.Q[current_state][max_Q]))
+
         if reward > 2:
             self.Q[current_state][self.A.index(action)] = \
                 (1-alpha)*self.Q[current_state][self.A.index(action)] + \
                 (alpha * reward)
+
             self.results.append(('Win!',self.init_deadline))  ## track wins
             print 'results:',self.results
+
+            self.optimal_policy_used.append((sum(self.optimal_action_used), len(self.optimal_action_used)))  ## track optimal policy moves
+            print 'optimal policy used: {}'.format(self.optimal_policy_used)
+
         else:
             if deadline == 0:
                 self.results.append(('Failed',self.init_deadline))  ## track fails
@@ -121,6 +152,7 @@ class LearningAgent(Agent):
 
         print "----\nQ({}): {}".format(len(self.Q), self.Q)  ## [debug]
         print "----\nLearningAgent.update({}): deadline = {}/{}, inputs = {}, action = {}, reward = {}".format(t, deadline, self.init_deadline, inputs, action, reward)  # [debug]
+        print "============"
 
 
 def run():
@@ -133,7 +165,7 @@ def run():
 
     # Now simulate it
     sim = Simulator(e)
-    sim.run(n_trials=40)  # press Esc or close pygame window to quit
+    sim.run(n_trials=30)  # press Esc or close pygame window to quit
 
 
 if __name__ == '__main__':
